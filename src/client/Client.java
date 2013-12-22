a8b2c4109fa003aef516430de143fe6ed9c5e4c3
krz.gdk@gmail.com <>
2013-12-22 23:11:39 +0100

    init

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,12 +4,32 @@
  */
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Krzysiek
  */
 public class Client{
     public static void main(String[] args){
-        
+        try (Socket server = new Socket("localhost", 7777)) {
+            PrintWriter to_server = new PrintWriter(server.getOutputStream(), true);
+            BufferedReader from_server = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            to_server.println("aaa");
+            System.out.println(from_server.readLine());
+            to_server.println("bbb");
+            System.out.println(from_server.readLine());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
