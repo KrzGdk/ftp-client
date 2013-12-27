3657ca892593589834438a915014ef3e5867cd2a
krz.gdk@gmail.com <>
2013-12-27 21:20:01 +0100

    sending requests

--- src/client/Client.java
+++ src/client/Client.java
@@ -19,13 +19,17 @@ import java.util.logging.Logger;
  */
 public class Client{
     public static void main(String[] args){
-        try (Socket server = new Socket("localhost", 7777)) {
-            PrintWriter to_server = new PrintWriter(server.getOutputStream(), true);
-            BufferedReader from_server = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            to_server.println("aaa");
-            System.out.println(from_server.readLine());
-            to_server.println("bbb");
-            System.out.println(from_server.readLine());
+        try (Socket server = new Socket("localhost", 21)) {
+            PrintWriter toServer = new PrintWriter(server.getOutputStream(), true);
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader fromServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            String command, response;
+            while(true){
+                command = stdin.readLine();
+                toServer.println(command);
+                response = fromServer.readLine();
+                System.out.println(response);
+            }
         } catch (UnknownHostException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
