aa46a43f18db8a5d19c03ff8caa2bd6f24bd9e15
krz.gdk@gmail.com <>
2013-12-30 00:42:09 +0100

    active retrieving file implemented

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,9 +5,12 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
@@ -18,6 +21,9 @@ import java.util.logging.Logger;
  * @author Krzysiek
  */
 public class Client{
+    private static ServerSocket dataSocket = null;
+    private static int clientPort;
+    
     public static void main(String[] args){
         try (Socket server = new Socket("localhost", 21)) {
             PrintWriter toServer = new PrintWriter(server.getOutputStream(), true);
@@ -29,6 +35,31 @@ public class Client{
                 toServer.println(command);
                 response = fromServer.readLine();
                 System.out.println(response);
+                if(command.startsWith("PORT ")){
+                    String address = command.substring(5);
+                    String[] splitAddress = address.split(",");
+                    int p1 = Integer.parseInt(splitAddress[4]);
+                    int p2 = Integer.parseInt(splitAddress[5]);
+                    clientPort = (p1 << 8) + p2; 
+                    if(dataSocket == null){
+                        dataSocket = new ServerSocket(clientPort);
+                    }
+                }
+                else if(command.startsWith("RETR ")){
+                    String filename = command.substring(5);
+                    
+                    RandomAccessFile file = new RandomAccessFile((filename),"rw");
+                    
+                    Socket dataChannel = dataSocket.accept();
+                    DataInputStream dataIn = new DataInputStream(dataChannel.getInputStream());
+
+                    int offset;
+                    byte[] data = new byte[1024];
+                    while( (offset = dataIn.read(data)) != -1){
+                        file.write(data, 0, offset);
+                    }
+                    file.close();
+                }
             }
         } catch (UnknownHostException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
